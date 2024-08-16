c97cd44f037e236bca52705d001adee92f8ab94a
Manik Singh <maniksingh92@gmail.com>
2024-08-16 12:18:13 -0700

    Add plugins for lsp

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,7 +8,13 @@ return {
     require('which-key').setup()
 
     require('which-key').add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
       { '<leader>f', group = '[F]ind' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
     }
   end,
 }
