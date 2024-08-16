409d1ad95ad49e262744b8a194b695b53fc70919
Manik Singh <maniksingh92@gmail.com>
2024-08-16 07:00:20 -0700

    Add plugin telescope

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,4 +4,11 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   event = 'VimEnter',
+  config = function()
+    require('which-key').setup()
+
+    require('which-key').add {
+      { '<leader>f', group = '[F]ind' },
+    }
+  end,
 }
